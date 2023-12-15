5e9e880b8e4f6bd3b9ad0afda646c112cb9129a0
Sophia <sophiafagleal@gmail.com>
2023-12-15 15:03:54 -0300

    esse negocio funciona so no xampp, não sei fazer autenticação http e so precisa disso pra funfar

--- autenticacao.php
+++ autenticacao.php
@@ -1,12 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Página Inicial</h1>
+    
+    <?php
+        $nome = $_POST["nome"];
+        $senha = $_POST["senha"]; 
 
-<?php 
-    $nome = $_POST["nome"];
-    $senha = $_POST["senha"];
-
-    if($nome === "Admin" and $senha === "admin")
-     header("Location: "."Siteadmin.php");
-    else if($nome === "Caio" and $senha === "caio")
-    header("Location: "."index.html");
-    else 
-    print("Nome/Senha inválidos");
+        if($nome == "debora" and $senha == "123"){
+            header("Location: "."inicialadm.html");
+        }elseif($nome == "maria" and $senha == "123"){
+            header("Location: "."index.html");
+        }else{
+            header("Location: "."trabnao.php");
+        }
+        
     ?>
+    
+</body>
+</html>
