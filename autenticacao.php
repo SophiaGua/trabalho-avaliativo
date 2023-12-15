4ee1dd94de13d2c89c8632937b93355ea6fe6cf7
Sophia <sophiafagleal@gmail.com>
2023-12-15 14:39:59 -0300

    esperimentando

--- autenticacao.php
+++ autenticacao.php
@@ -1,27 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Página Inicial</h1>
-    
-    <?php
-        $nome = $_POST["nome"];
-        $senha = $_POST["senha"]; 
 
-        if($nome == "adm" and $senha == "123"){
-            header("Location: "."index.html");
-        }elseif($nome == "Mariana" and $senha == "123"){
-            header("Location: "."inicialusu.php");
-        }elseif($nome == "Bibliotecario" and $senha == "123"){
-            header("Location: "."inicialbiblio.php");
-        }else{
-            print ("<p> senha incoreta, tente novamente!");
-        }
+<?php 
+    $nome = $_POST["nome"];
+    $senha = $_POST["senha"];
+
+    if($nome === "Admin" and $senha === "admin")
+     header("Location: "."Siteadmin.php");
+    else if($nome === "Caio" and $senha === "caio")
+    header("Location: "."index.html");
+    else 
+    print("Nome/Senha inválidos");
     ?>
-    
-</body>
-</html>
